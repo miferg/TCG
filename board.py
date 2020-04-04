82c0f0cb3fd35bbf221d64962b3862b923e211f3
Miguel F. Romero <43483471+miferg@users.noreply.github.com>
2020-04-04 13:20:43 -0600

    Update board.py

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python3
 
 import pygame
+import sys
 from network import Network
 from player import Player
 from cards import *
+
 
 width = 1000
 height = 800
@@ -16,7 +18,22 @@ back_filename = 'cards/429px-Magic_card_back.bmp'
 back = pygame.image.load(back_filename).convert_alpha()
 back = pygame.transform.scale(back,card_dims)
 
-cardnames = ['bc.bmp','dsf.bmp','swamp.bmp','swamp.bmp','ytp.bmp']
+grayback_filename = 'cards/429px-Magic_card_back_gray.bmp'
+grayback = pygame.image.load(grayback_filename).convert_alpha()
+grayback = pygame.transform.scale(grayback,card_dims)
+
+################################################################################
+#yawgmoth EDH
+lands = []
+for i in range(0,31):
+    lands.append('swamp.bmp')
+
+handle = open('yawgmoth_edh/ytp_edh.txt','r')
+decklist = handle.read().split('\n')[:-1]
+handle.close()
+
+deckdir = 'yawgmoth_edh/'
+################################################################################
 
 def redrawWindow(win,player, player2):
     win.fill((255,255,255))
@@ -33,8 +50,13 @@ def main():
 
     cardsindeck = []
 
-    for i in cardnames:
-        front = pygame.image.load('cards/'+i).convert_alpha()
+    for i in lands:
+        front = pygame.image.load('lands/'+i).convert_alpha()
+        front = pygame.transform.scale(front,card_dims)
+        cardsindeck.append(Card(0, 0, card_dims[0], card_dims[1], (20,20,20), back, front))
+
+    for i in decklist:
+        front = pygame.image.load(deckdir+i).convert_alpha()
         front = pygame.transform.scale(front,card_dims)
         cardsindeck.append(Card(0, 0, card_dims[0], card_dims[1], (20,20,20), back, front))
 
@@ -48,14 +70,18 @@ def main():
         clock.tick(60)
         #p2 = n.send(p)
 
+        mouse_x, mouse_y =  pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+                sys.exit()
 
             for crd in cardsinplay:
                 crd.drag(event)
                 crd.standby(event)
+                crd.put_on_deck(event, cardsinplay, decksinplay)
 
             for deck in decksinplay:
                 deck.drag(event)
@@ -64,16 +90,22 @@ def main():
         win.fill((255,255,255))
 
         for deck in decksinplay:
+            deck.manage()
             deck.print(win)
-            deck.shuffle()
             drawed = deck.draw()
             if drawed:
+                drawed.x = mouse_x
+                drawed.y = mouse_y
+                drawed.update()
                 cardsinplay.append(drawed)
+            newtoken = deck.token(mouse_x, mouse_y, grayback)
+            if newtoken:
+                cardsinplay.append(newtoken)
 
-        if len(cardsinplay) > 0:
-            for crd in cardsinplay:
-                crd.transform()
-                crd.print(win)
+        #if len(cardsinplay) > 0:
+        for crd in cardsinplay:
+            crd.transform()
+            crd.print(win)
 
         #p.move()
         #redrawWindow(win, p, p2)
